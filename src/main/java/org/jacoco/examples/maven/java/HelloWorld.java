076df1cfdf71a2323ff9c4063d73a86aea0c60fa
Karpov <a.i.karpov98@gmail.com>
2020-11-20 23:11:37 +0300

    new test

--- src/main/java/org/jacoco/examples/maven/java/HelloWorld.java
+++ src/main/java/org/jacoco/examples/maven/java/HelloWorld.java
@@ -3,7 +3,7 @@ package org.jacoco.examples.maven.java;
 public class HelloWorld {
 	
 	public static void main(String[] args){
-		System.out.println("Hello world");
+		System.out.println("Hello  world");
 	}
 
 }
